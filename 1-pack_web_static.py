ec06b918a3b287170acac6de9a0c0196333b5c87
PhenomenalAI <diamondinyang@gmail.com>
2023-10-06 13:29:16 +0000

    fabric script that generates a .tgz archive from the contents..

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,19 +5,16 @@ Fabric script to generate a .tgz archive from the contents of the web_static fol
 
 from fabric.api import local
 from datetime import datetime
-import os
 
 def do_pack():
     """
     Creates a .tgz archive from the contents of the web_static folder.
     """
     # Create the "versions" directory if it doesn't exist
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
+    local("mkdir -p versions")
 
     # Create the name of the archive file
-    now = datetime.now()
-    timestamp = now.strftime("%Y%m%d%H%M%S")
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = "versions/web_static_{}.tgz".format(timestamp)
 
     # Compress the web_static folder into a .tgz archive
