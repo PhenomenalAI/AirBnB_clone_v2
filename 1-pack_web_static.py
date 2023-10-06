d68bb32f9f6f628622af3aca34db04225c5194ed
PhenomenalAI <diamondinyang@gmail.com>
2023-10-06 13:13:23 +0000

    fabric script that generates a .tgz archive from the contents..

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-"""Generates a .tgz archive from the contents of the web_static folder
-   using the function do_pack"""
+"""
+Fabric script to generate a .tgz archive from the contents of the web_static folder.
+"""
 
-
-from fabric.api import *
+from fabric.api import local
 from datetime import datetime
-
+import os
 
 def do_pack():
-    """Generates a .tgz archive from the contents of web_static folder"""
-    local("sudo mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "versions/web_static_{}.tgz".format(date)
-    result = local("sudo tar -cvzf {} web_static".format(filename))
+    """
+    Creates a .tgz archive from the contents of the web_static folder.
+    """
+    # Create the "versions" directory if it doesn't exist
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    # Create the name of the archive file
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    archive_name = "versions/web_static_{}.tgz".format(timestamp)
+
+    # Compress the web_static folder into a .tgz archive
+    result = local("tar -cvzf {} web_static".format(archive_name))
+
+    # Check if the archive was created successfully
     if result.succeeded:
-        return filename
+        return archive_name
     else:
         return None
