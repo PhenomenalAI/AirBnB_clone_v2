bc04cdffa08835d1406c440d0e7b4c06668ff168
PhenomenalAI <diamondinyang@gmail.com>
2023-10-20 03:50:36 -0700

    ...routes that displays HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,16 +11,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Defines a route for the root URL ("/") and set strict_slashes to False
-@app.route('/', strict_slashes = False)
-def hello_hbnb():
-	"""Displays Hello HBNB"""
-	return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes = False)
+# Defines a route for the root URL ("/") and set strict_slashes to False
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
 def displays_str():
-	"""Displays 'HBNB'."""
-	return "HBNB"
+    """Displays 'HBNB'."""
+    return "HBNB"
+
 
 if __name__ == "__main__":
-	app.run(host= "0.0.0.0", port = 5000)
+    app.run(host="0.0.0.0", port=5000)
