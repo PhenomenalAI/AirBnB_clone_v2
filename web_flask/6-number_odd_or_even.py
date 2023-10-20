279481dbab47656bfbbe34789870e14d4bb81985
PhenomenalAI <diamondinyang@gmail.com>
2023-10-20 09:57:00 -0700

    ...display odd/even numbers if they are numbers

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -10,6 +10,8 @@ Routes:
 from flask import Flask
 
 app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 
 # Defines a route for the root URL ("/") and set strict_slashes to False
