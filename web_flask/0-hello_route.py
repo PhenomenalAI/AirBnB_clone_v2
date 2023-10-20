c805233d8043e8c265a853941ab900eb082e8493
PhenomenalAI <diamondinyang@gmail.com>
2023-10-20 03:42:26 -0700

    starts a web flak application...

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,10 +11,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 # Defines a route for the root URL ("/") and set strict_slashes to False
-@app.route('/', strict_slashes = False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
