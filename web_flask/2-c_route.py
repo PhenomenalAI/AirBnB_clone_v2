8db962dfa6bbb0f6b4bfb69ceaac84d745a84d4f
PhenomenalAI <diamondinyang@gmail.com>
2023-10-20 03:56:33 -0700

    ....displays 'C' and a text

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,22 +11,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 # Defines a route for the root URL ("/") and set strict_slashes to False
-@app.route('/', strict_slashes = False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-        """Displays Hello HBNB"""
-        return "Hello HBNB!"
+    """Displays Hello HBNB"""
+    return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes = False)
+
+@app.route("/hbnb", strict_slashes=False)
 def displays_str():
-        """Displays 'HBNB'."""
-        return "HBNB"
+    """Displays 'HBNB'."""
+    return "HBNB"
 
-@app.route("/c/<text>", strict_slahes = False)
+
+@app.route("/c/<text>", strict_slahes=False)
 def val_text(text):
-	"""Displays 'C' followed by value of <text>"""
-	text = text.replace("_", " ")
-	return "C {}".format(text)
+    """Displays 'C' followed by value of <text>"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
 
 if __name__ == "__main__":
-        app.run(host= "0.0.0.0", port = 5000)
+    app.run(host="0.0.0.0", port=5000)
