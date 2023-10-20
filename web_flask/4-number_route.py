4ff0f4220843f98d18d13af4fceea8999abac490
PhenomenalAI <diamondinyang@gmail.com>
2023-10-20 04:48:35 -0700

    ...adds number route that shows a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Defines a route for the root URL ("/") and set strict_slashes to False
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def displays_str():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def val_text(text):
+    """Displays 'C' followed by value of <text>"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def displays_py(text="is cool"):
+    """Displays 'Python' followed by value of text"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def displays_int(n):
+    """Displays 'n is anumber' if n is an integer"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
